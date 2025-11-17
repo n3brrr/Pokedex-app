63378b2872a6bc08ae3e414c61ea451a2b1ac459
nebur <neburtl837@gmail.com>
2025-11-17 19:43:15 +0100

    Add PokemonCard component and display pokemon grid

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,10 @@ import { getPokemonList, getPokemonById } from "@/services/pokemon";
 import PokemonCard from "@/components/PokemonCard";
 
 export default async function Home() {
-  // Probar función 1: Obtener lista
   const lista = await getPokemonList(10);
-  console.log("📋 Lista de 10 pokémon:", lista);
-  // Probar función 2: Obtener Pikachu
-  const pikachu = await getPokemonById(25);
-  console.log("⚡ Pikachu:", pikachu);
 
   return (
-    <div className="flex items-center justify-center h-screen flex-col gap-4">
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 m-5">
       {lista.results.map((pokemon) => (
         <PokemonCard key={pokemon.url} name={pokemon.name} url={pokemon.url} />
       ))}
